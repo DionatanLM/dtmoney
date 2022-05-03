a63484ff4a283655c59f5ec7ad63239da85f4285
dionatanlm <dionatanlm12271@gmail.com>
2022-05-03 06:22:30 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import logoImg from '../../assets/logo.svg'
+import Modal from 'react-modal';
+import { Container, Content } from './styles'
+import { useState } from 'react';
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal}: HeaderProps) {
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+        
+      </Content>
+    </Container>
+  )
+}
